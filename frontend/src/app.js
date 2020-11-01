00f814ca7a882553673e14d8baed7f73ab68926d
Leonardo Luiz Gava <hi@llgava.net>
2020-11-01 16:26:40 -0400

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,18 @@
+// Components
+import Header from './components/Header';
+import SearchBox from './components/SearchBox';
+
+// Containers
+import { GlobalStyle } from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SearchBox />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
