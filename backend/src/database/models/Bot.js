83b79835649a5791f731649552aba222c010a54c
Leonardo Luiz Gava <hi@llgava.net>
2020-11-14 19:50:14 -0400

    Update Bot.js

--- backend/src/database/models/Bot.js
+++ backend/src/database/models/Bot.js
@@ -1,6 +1,8 @@
 const Mongoose = require('mongoose');
+const _id = Mongoose.Schema.Types.ObjectId;
 
 const BotSchema = new Mongoose.Schema({
+  _id: { type: _id, required: true, unique: true, select: false },
   owner_id: { type: String, require: true },
   client_id: { type: String, require: true, unique: true },
   name: { type: String, require: true },
